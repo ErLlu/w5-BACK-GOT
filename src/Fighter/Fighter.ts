4b948773df73546c58ac44d1ecced649a10d1f34
Eric Lluis <ericlluis84@gmail.com>
2024-04-30 21:02:14 +0200

    Add Fighter class

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -9,7 +9,11 @@ export class Fighter extends Character implements FighterStructure {
   public weapon: string;
   public skillLevel: Level;
 
-  constructor(character: CharacterBasic, weapon: string, skillLevel: Level) {
+  public constructor(
+    character: CharacterBasic,
+    weapon: string,
+    skillLevel: Level,
+  ) {
     super(character.name, character.lastName, character.age);
     this.weapon = weapon;
     this.skillLevel = skillLevel;
