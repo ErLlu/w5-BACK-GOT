d21532ee09fb0d5f18e14de3e3532c2659ad5b64
Eric Lluis <ericlluis84@gmail.com>
2024-04-30 20:50:52 +0200

    Add Fighter class

---
+++ src/Fighter/Fighter.ts
@@ -0,0 +1,21 @@
+import { Character } from "../Character/Character.js";
+import {
+  type FighterStructure,
+  type CharacterBasic,
+  type Level,
+} from "../Character/types.js";
+
+export class Fighter extends Character implements FighterStructure {
+  public weapon: string;
+  public skillLevel: Level;
+
+  constructor(character: CharacterBasic, weapon: string, skillLevel: Level) {
+    super(character.name, character.lastName, character.age);
+    this.weapon = weapon;
+    this.skillLevel = skillLevel;
+  }
+
+  speak() {
+    return "Primero pego y luego pregunto";
+  }
+}
